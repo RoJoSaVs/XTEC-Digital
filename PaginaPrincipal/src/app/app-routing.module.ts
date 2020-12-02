75891fe72f9c0b231ada8766882ea5e8051d9bf1
Calquito <allancalqui@gmail.com>
2020-12-01 19:06:38 -0600

    Login links to components working

--- PaginaPrincipal/src/app/app-routing.module.ts
+++ PaginaPrincipal/src/app/app-routing.module.ts
@@ -1,15 +1,23 @@
+
 //imports de bibliotecas de angular
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { PaginaDeInicioComponent} from './pagina-de-inicio/pagina-de-inicio.component'
 
-//imports de componentes
-import {ConnectionService} from 'src/app/connection.service'
 
+//imports de componentes
+import { ConnectionService} from 'src/app/connection.service'
+import { LoginEstudianteComponent } from './login-estudiante/login-estudiante.component';
+import { LoginAdministradorComponent} from './login-administrador/login-administrador.component'
+import { LoginProfesorComponent } from './login-profesor/login-profesor.component';
 
 
 const routes: Routes = [
-  { path: '', component: PaginaDeInicioComponent}
+  { path: '', component: PaginaDeInicioComponent},
+  { path: 'login-profesor', component: LoginProfesorComponent},
+  { path: 'login-estudiante', component: LoginEstudianteComponent},
+  { path: 'login-administrador', component: LoginAdministradorComponent}
+
 ];
 
 @NgModule({
